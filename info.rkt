31332584e17a0c6d96349ea176ecfc8a2d987ed5
OscarOrtiz05 <a01571904@tec.mx>
2026-05-04 14:42:51 -0600

    Support as a racket library added

---
+++ info.rkt
@@ -0,0 +1,8 @@
+#lang info
+
+(define name "mu-recursive")
+(define version "0.1")
+(define deps '("base"))
+(define build-deps '())
+(define pkg-desc "A μ-recursive function library implemented in Racket")
+(define pkg-authors '(Oscar Ortiz))
